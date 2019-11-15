4f4b4e321ca7a724347e41c7fd1a0b1d235e9d4a
elior <ZennenB@bitbucket>
2019-11-15 11:07:01 +0100

    create table FicheFrais

---
+++ src/Entity/FicheFrais.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\FicheFraisRepository")
+ */
+class FicheFrais
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbJustificatifs;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $montant;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateModif;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNbJustificatifs(): ?int
+    {
+        return $this->nbJustificatifs;
+    }
+
+    public function setNbJustificatifs(int $nbJustificatifs): self
+    {
+        $this->nbJustificatifs = $nbJustificatifs;
+
+        return $this;
+    }
+
+    public function getMontant(): ?int
+    {
+        return $this->montant;
+    }
+
+    public function setMontant(int $montant): self
+    {
+        $this->montant = $montant;
+
+        return $this;
+    }
+
+    public function getDateModif(): ?\DateTimeInterface
+    {
+        return $this->dateModif;
+    }
+
+    public function setDateModif(\DateTimeInterface $dateModif): self
+    {
+        $this->dateModif = $dateModif;
+
+        return $this;
+    }
+}
